4c8c3f736682d52848d949ee94f11aba22f62715
Igor <benefacta@gmail.com>
2021-02-01 22:41:20 +0200

    add projects to sixth task

---
+++ sixth_homework_convert/TempConvertor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace sixth_homework_convert
+{
+    public class TempConvertor
+    {
+        public int ConvertFromCtoF(int c) {
+            return Convert.ToInt32((c*9/5)+32);
+        }
+        public int ConvertFromFtoC(int f) {
+            return Convert.ToInt32((f-32)*5/9);
+        }
+    }
+}
